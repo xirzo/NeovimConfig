cc34b54d7429dae97481ecbd379dbc685758bb7d
xirzo <easttem@gmail.com>
2024-07-31 15:53:58 +0300

    Adding new themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
   { "shaunsingh/nord.nvim" },
   { "ellisonleao/gruvbox.nvim" },
+  { "dgox16/oldworld.nvim" },
+  { "xero/miasma.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nord",
+      colorscheme = "oldworld",
     },
   },
 }
