278679e32c0772ebb05206d500d0a8243e9d29fa
xirzo <easttem@gmail.com>
2024-12-07 13:03:08 +0300

    Colorsceme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,31 @@
 return {
-  { "shaunsingh/nord.nvim" },
-  { "ellisonleao/gruvbox.nvim" },
-  { "dgox16/oldworld.nvim" },
-  { "xero/miasma.nvim" },
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+        },
+      })
+
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
   },
 }
