dd89c1c9a89a96bb0119987316cd37e82fe63c51
xirzo <easttem@gmail.com>
2025-01-16 12:07:38 +0300

    Smart indent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.mouse = ""
+local opt = vim.opt
+
+opt.mouse = ""
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.smartindent = true
