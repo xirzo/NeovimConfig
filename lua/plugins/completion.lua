6f0f284ea11926163c631247ee39718d968e47ad
xirzo <easttem@gmail.com>
2025-02-25 09:37:51 +0300

    Fixed autocompletion

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,143 @@
+return {
+  {
+    "saghen/blink.cmp",
+    version = not vim.g.lazyvim_blink_main and "*",
+    build = vim.g.lazyvim_blink_main and "cargo build --release",
+    opts_extend = {
+      "sources.completion.enabled_providers",
+      "sources.compat",
+      "sources.default",
+    },
+    event = "InsertEnter",
+    dependencies = {
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        config = function()
+          local ls = require("luasnip")
+          require("luasnip.loaders.from_vscode").load({ paths = { "./snippets" } })
+          ls.config.setup({
+            history = true,
+            updateevents = "TextChanged,TextChangedI",
+          })
+        end,
+      },
+      "rafamadriz/friendly-snippets",
+      {
+        "saghen/blink.compat",
+        optional = true,
+        opts = {},
+        version = not vim.g.lazyvim_blink_main and "*",
+      },
+    },
+
+    opts = {
+      -- previous configurations remain the same until keymap
+      snippets = {
+        preset = "luasnip",
+      },
+      appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        accept = {
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+        ghost_text = {
+          enabled = vim.g.ai_cmp,
+        },
+      },
+
+      sources = {
+        compat = {},
+        default = { "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lsp = {
+            name = "LSP",
+            module = "blink.cmp.sources.lsp",
+            transform_items = function(_, items)
+              return vim.tbl_filter(function(item)
+                return item.kind ~= require("blink.cmp.types").CompletionItemKind.Text
+              end, items)
+            end,
+          },
+        },
+      },
+
+      cmdline = {
+        enabled = true,
+        sources = function()
+          local type = vim.fn.getcmdtype()
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          if type == ":" or type == "@" then
+            return { "cmdline" }
+          end
+          return {}
+        end,
+      },
+
+      keymap = {
+        preset = "enter",
+        ["<C-j>"] = { "select_next" },
+        ["<C-k>"] = { "select_prev" },
+        ["<C-y>"] = { "select_and_accept" },
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        ["<C-Space>"] = { "show", "show_documentation" },
+        ["<C-e>"] = { "cancel", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      },
+    },
+
+    config = function(_, opts)
+      -- Rest of the config remains the same
+      local enabled = opts.sources.default
+      for _, source in ipairs(opts.sources.compat or {}) do
+        opts.sources.providers[source] = vim.tbl_deep_extend(
+          "force",
+          { name = source, module = "blink.compat.source" },
+          opts.sources.providers[source] or {}
+        )
+        if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+          table.insert(enabled, source)
+        end
+      end
+
+      opts.sources.compat = nil
+
+      for _, provider in pairs(opts.sources.providers or {}) do
+        if provider.kind then
+          local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+          local kind_idx = #CompletionItemKind + 1
+
+          CompletionItemKind[kind_idx] = provider.kind
+          CompletionItemKind[provider.kind] = kind_idx
+
+          local transform_items = provider.transform_items
+          provider.transform_items = function(ctx, items)
+            items = transform_items and transform_items(ctx, items) or items
+            for _, item in ipairs(items) do
+              item.kind = kind_idx or item.kind
+            end
+            return items
+          end
+
+          provider.kind = nil
+        end
+      end
+
+      require("blink.cmp").setup(opts)
+    end,
+  },
+}
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,33 +1,51 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
 
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
         },
-        mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "luasnip" },
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
+
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    -- ["<Tab>"] = cmp.mapping(function(fallback)
+                    --   if cmp.visible() then
+                    --     cmp.select_next_item()
+                    --   elseif luasnip.expand_or_jumpable() then
+                    --     luasnip.expand_or_jump()
+                    --   else
+                    --     fallback()
+                    --   end
+                    -- end, { "i", "s" }),
+                    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    --   if cmp.visible() then
+                    --     cmp.select_prev_item()
+                    --   elseif luasnip.jumpable(-1) then
+                    --     luasnip.jump(-1)
+                    --   else
+                    --     fallback()
+                    --   end
+                    -- end, { "i", "s" }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "luasnip" },
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            })
+        end,
+    },
 }
