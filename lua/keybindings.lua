eb117cb2e0f2d6c82f5f8b0d98c5c7701b833270
xirzo <easttem@gmail.com>
2025-06-28 12:26:57 +0300

    Replace old config

---
+++ lua/keybindings.lua
@@ -0,0 +1,20 @@
+local moveline = require('moveline')
+
+-- moving lines with alt
+vim.keymap.set('n', '<M-k>', moveline.up)
+vim.keymap.set('n', '<M-j>', moveline.down)
+vim.keymap.set('v', '<M-k>', moveline.block_up)
+vim.keymap.set('v', '<M-j>', moveline.block_down)
+
+-- telesope
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
+vim.keymap.set("n", "<leader>/", "<cmd>Telescope live_grep<cr>", opts)
+
+-- buffers navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+
+-- Terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts)
