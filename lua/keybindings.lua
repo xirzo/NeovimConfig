1c10359b7f4b444830449114c573cb422f7b4c3d
xirzo <easttem@gmail.com>
2025-06-29 23:16:32 +0300

    Harpoon 2

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -26,6 +26,22 @@ vim.keymap.set("n", "<s-z>", ":Compile<Return>", { desc = "Compile" })
 vim.keymap.set("n", "<c-z>", ":Recompile<Return>", { desc = "Recompile" })
 
 -- Buffers
-vim.keymap.set("n", "<tab>", ":bnext<Return>", opts)
-vim.keymap.set("n", "<s-tab>", ":bprev<Return>", opts)
+-- vim.keymap.set("n", "<tab>", ":bnext<Return>", opts)
+-- vim.keymap.set("n", "<s-tab>", ":bprev<Return>", opts)
 vim.keymap.set("n", "<s-q>", ":bp|bd #<Return>", opts)
+
+-- Harpoon
+local harpoon = require("harpoon")
+
+harpoon:setup()
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<tab>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<s-tab>", function() harpoon:list():next() end)
---
+++ lua/plugins/harpoon2.lua
@@ -0,0 +1,7 @@
+-- TODO: maybe add Telescope as ui for harpoon [guide](https://github.com/ThePrimeagen/harpoon/tree/harpoon2)
+
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+}
