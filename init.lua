2f5c0fa21444d32a984b2d53455c6a2b1d4a461f
xirzo <easttem@gmail.com>
2025-08-11 13:49:41 +0300

    feat: autoformat on save

--- init.lua
+++ init.lua
@@ -29,3 +29,15 @@ vim.opt.wrap = false
 vim.o.background = "dark"
 vim.opt.termguicolors = true
 vim.cmd([[colorscheme moonfly]])
+
+-- auto format
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        local mode = vim.api.nvim_get_mode().mode
+        local filetype = vim.bo.filetype
+        if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+            vim.cmd('lua vim.lsp.buf.format()')
+        else
+        end
+    end
+})
