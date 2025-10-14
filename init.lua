d9e890442f230a8148c3b15d760a51951a2dedaa
xirzo <easttem@gmail.com>
2025-10-14 23:24:12 +0300

    feat: smartcase

--- init.lua
+++ init.lua
@@ -5,6 +5,8 @@ vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.tabstop=4
 vim.opt.shiftwidth=4
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
 
 -- cursor line indenting
 vim.opt.number = true
