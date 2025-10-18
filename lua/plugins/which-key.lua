28e0111ab08cf20d495610de5d0368e5146b3aeb
xirzo <easttem@gmail.com>
2025-10-18 11:34:50 +0300

    feat: remove which-key delay

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,7 +1,15 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = { },
+  opts = {
+    layout = {
+      height = { min = 4, max = 25 },
+      width = { min = 20, max = 50 },
+      spacing = 3,
+      align = "left",
+    },
+    delay = 0,
+  },
   keys = {
     {
       "<leader>?",
